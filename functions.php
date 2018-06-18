f2a6b1ced96257a0a891e587fe144d1f37337dd0
pinklightsaber <ctorres@idemar.cl>
2018-06-18 17:06:45 -0400

    adicionando bootstrap a wp

--- functions.php
+++ functions.php
@@ -5,9 +5,23 @@
 	function dl_enqueue_style(){
 		$theme_data = wp_get_theme();
 
-		//Registrar estilos
-		wp_register_style('main', get_parent_theme_file_uri('/css/main.css'),null, $theme_data->get('1.0'));
-		//llamar estilos
+		//Registrar estilos de Bootstrap
+		wp_register_style(
+			'bootstrap_css',
+			'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+			null, 
+			$theme_data->get('3.3.7'));
+
+		//Registrar estilos CSS "main.css"
+		wp_register_style(
+			'main', 
+			get_parent_theme_file_uri('/css/main.css'),
+			null, 
+			$theme_data->get('1.0'));
+
+		//llamar estilos bootstrap_css
+		wp_enqueue_style('bootstrap_css');
+		//llamar estilos CSS main
 		wp_enqueue_style('main');
 	}
 
@@ -17,6 +31,33 @@
 
 	//Registrar JS
 	function dl_enqueue_scripts() {
-		
+		$theme_data = wp_get_theme();
+
+		//Registrar JS de Bootstrap
+		wp_register_script(
+			'bootstrap_js', 
+			'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', 
+			array('jquery'), //wp incluye jquery
+			'3.3.7', 
+			true); //true footer, false header
+
+		//Registrar scripts
+		wp_register_script(
+			'scripts', 
+			get_parent_theme_file_uri('/js/script.js'), 
+			null, 
+			'1.0', 
+			true); //true footer, false header
+
+		//llamar script de jquery
+		wp_enqueue_script('jquery');
+
+		//llamar script de js de bootstrap
+		wp_enqueue_script('bootstrap_js');
+
+		//llamar script
+		wp_enqueue_script('scripts');
 	}
+
+	add_action('wp_enqueue_script', 'dl_enqueue_scripts')
 ?>
